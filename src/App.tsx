77b4da9f0d17c664e91a61a28f048fa2cadade71
wingrove210 <vladidermkov330@gmail.com>
2025-07-20 19:00:01 +0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Catalog from './components/Catalog/Catalog'
-import Details from './pages/Details/Details'
+// import Catalog from './components/Catalog/Catalog'
+// import Details from './pages/Details/Details'
 import Home from './pages/Home/Home'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Catalog/>}/>
-        <Route path='/product/:id' element={<Details/>}/>
-        <Route path='/home' element={<Home/>}/>
+        {/* <Route path='/' element={<Catalog/>}/>
+        <Route path='/product/:id' element={<Details/>}/> */}
+        <Route path='/' element={<Home/>}/>
       </Routes>
     </BrowserRouter>
   )
